9b3bd491c6b245c1de3a67d2ac2071ac472d7ec6
Ivan-R-BS <ibinetsanchez@ucsd.edu>
2025-03-02 23:11:18 -0800

    still updating

---
+++ form.js
@@ -0,0 +1,10 @@
+const name = document.getElementById('yourname')
+const email = document.getElementById('email')
+const comment = document.getElementById('comments')
+const error = document.getElementById('errormessage')
+const info = document.getElementById('infomessage')
+const form = document.getElementById('formpost')
+
+form.addEventListener('submit', (e) ){
+    let messages = []
+}
